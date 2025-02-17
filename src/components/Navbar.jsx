04c3389f0b531646c21a30252cd85f2b76972195
Daniel <danielmommsen2@gmail.com>
2025-02-17 09:58:34 +0200

    Updated the navigation bars styles to look more unifom and added hover styling over the links in the naigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,62 @@
 import React from "react";
-import { Navbar, Nav, Badge } from "react-bootstrap";
+import { Navbar, Nav, Badge, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import { useWishlist } from "../context/WishlistContext"; // Import Wishlist context
-import "../styles/CustomNavbar.css"; // Custom CSS file for additional styling
-import { FaShoppingCart, FaHeart } from "react-icons/fa"; // Import icons
+import { useWishlist } from "../context/WishlistContext";
+import "../styles/CustomNavbar.css";
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
 
 const CustomNavbar = () => {
-  const { cart } = useCart(); // Get cart items
-  const { wishlist } = useWishlist(); // Get wishlist items
+  const { cart } = useCart();
+  const { wishlist } = useWishlist();
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
-      <Navbar.Brand as={Link} to="/" className="brand-logo">
-        <img
-          src="/goshoplogo-512x512.png" // Assuming logo.png is in the public folder
-          alt="GoShop Logo"
-          width="50"
-          height="50"
-          className="d-inline-block align-top"
-        />
-        <span className="brand-name">GoShop</span>
-      </Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar shadow">
+      <Container>
+        {/* Logo and Brand Name */}
+        <Navbar.Brand as={Link} to="/" className="brand-logo d-flex align-items-center">
+          <img
+            src="/goshoplogo-512x512.png"
+            alt="GoShop Logo"
+            width="60"
+            height="60"
+            className="me-2"
+          />
+          <span href="/" className="brand-name fw-bold">GoShop</span>
+        </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="navbar-nav" />
-      <Navbar.Collapse id="navbar-nav">
-        <Nav className="ms-auto">
-          {/* Wishlist Icon */}
-          <Nav.Link as={Link} to="/wishlist" className="wishlist-link">
-            <FaHeart className="wishlist-icon" /> Wishlist{" "}
-            {wishlist.length > 0 && (
-              <Badge bg="danger" className="wishlist-badge">
-                {wishlist.length}
-              </Badge>
-            )}
-          </Nav.Link>
+        <Navbar.Toggle aria-controls="navbar-nav" className="border-0" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto align-items-center">
+            {/* Wishlist */}
+            <Nav.Link as={Link} to="/wishlist" className="nav-link wishlist-link">
+              <FaHeart className="nav-icon wishlist-icon" />
+              Wishlist{" "}
+              {wishlist.length > 0 && (
+                <Badge bg="danger" className="badge-count">
+                  {wishlist.length}
+                </Badge>
+              )}
+            </Nav.Link>
 
-          {/* Cart Icon */}
-          <Nav.Link as={Link} to="/cart" className="cart-link">
-            <FaShoppingCart className="cart-icon" /> Cart{" "}
-            {cart.length > 0 && (
-              <Badge bg="light" text="dark" className="cart-badge">
-                {cart.length}
-              </Badge>
-            )}
-          </Nav.Link>
+            {/* Cart */}
+            <Nav.Link as={Link} to="/cart" className="nav-link cart-link">
+              <FaShoppingCart className="nav-icon cart-icon" />
+              Cart{" "}
+              {cart.length > 0 && (
+                <Badge bg="light" text="dark" className="badge-count">
+                  {cart.length}
+                </Badge>
+              )}
+            </Nav.Link>
 
-          {/* Products Page Link */}
-          <Nav.Link as={Link} to="/products" className="products-link">
-            Products
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+            {/* Products */}
+            <Nav.Link as={Link} to="/products" className="nav-link products-link fw-bold">
+              Products
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
